f6860221161b67dc8b8a8c15e3aa0aeea30e39cb
Steve <stevesiddu49@gmail.com>
2021-06-26 13:45:49 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 
 import './App.css';
 import styled from 'styled-components';
-// import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
-// import Home from './components/Home';
-// import Topics from './components/Topics';
-// import Quiz from './components/Quiz';
-import Header from './components/ui/Header';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
+import Home from './components/Home';
+import Topics from './components/Topics';
+import Quiz from './components/Quiz';
+// import Header from './components/ui/Header';
 
 function App() {
     
     const DivApp = styled.div`
         position:relative;
     `;
-
+    const req='http://Steve-siddu/Quiz';
 
     return (
         <DivApp>
-            {/* <Router> */}
-                <Header />
-                {/* <Switch>
-                    <Route path='/' exact component={Home}></Route>
-                    <Route path='/topics' exact component={Topics}></Route>
-                    <Route path='/topics/:id' component={Quiz}></Route>
-                    <Route path='/' render={()=><div>404</div>}></Route>
-                </Switch> */}
-            {/* // </Router> */}
+            <Router>
+                {/* <Header /> */}
+                <Switch>
+                    <Route path={req+'/'} exact component={Home}></Route>
+                    <Route path={req+'/topics'} exact component={Topics}></Route>
+                    <Route path={req+'/topics/:id'} component={Quiz}></Route>
+                    <Route path='/' render={()=>{
+                        {console.log('thappu undhi')}
+                        <div>404</div>}}>
+                    </Route>
+                </Switch>
+            </Router>
         </DivApp> 
     );
 }
